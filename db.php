1ef0e576e059980b16a67c362272e69d681900bb
kj6560 <kj6560@gmail.com>
2022-06-14 12:10:48 +0530

    modified db

--- db.php
+++ db.php
@@ -20,16 +20,14 @@ function getUser($user_id,$conn){
 }
 
 function getConference($conn,$user,$conference_type){
-    print_r($user);
     $result = array();
     if($conference_type==1){
         $query = "select * from conference where conference_type= ".$conference_type." and is_available=1 and (conference_by = ".$user[0]." or conference_for=".$user[0].")";
     }else if($conference_type==2){
         $query = "select * from conference where conference_type= ".$conference_type." and is_available=1 and (conference_by = ".$user[0]." or conference_for in(".$user[0]."))";
     }
-    print_r($query);
     $data = mysqli_query($conn,$query);
-    while($row=mysqli_fetch_row($data)){
+    while($row=mysqli_fetch_assoc($data)){
         $result = $row;
     }
     return !empty($result)?$result:false;
