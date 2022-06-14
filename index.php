586c99ffea1835a347104ded0be196960105164d
kj6560 <kj6560@gmail.com>
2022-06-14 16:37:24 +0530

    sfjkslk

--- index.php
+++ index.php
@@ -147,7 +147,7 @@ if ($conn && $user_id) {
 							<br />
 						</div>
 					</div>
-					
+
 					<div class="bg-dark d-flex flex-column h-100 w-100 p-4" style="position: fixed;top: 0; left: 0;">
 						<h4 class="text-white text-center"></h4>
 						<div class="flex-grow-1">
@@ -155,16 +155,17 @@ if ($conn && $user_id) {
 							<div class="row no-gutters h-100" id="remoteVideos">
 								<div id="remoteAudios"></div>
 							</div>
-						</div>
-						<div class="flex-grow-1 text-right">
-						<div id="videoMedia" class="hidden">
-							<h4><i class="fab fa-youtube"></i> <?php echo $current_user['name'] ?></h4>
-							<div id="localMedia" class="containers">
-							</div>
-							<br />
+							<div class="flex-grow-1 text-right">
+							<div id="videoMedia" class="hidden">
+								<h4><i class="fab fa-youtube"></i> <?php echo $current_user['name'] ?></h4>
+								<div id="localMedia" class="containers">
+								</div>
+								<br />
 
+							</div>
 						</div>
-					</div>
+						</div>
+						
 						<div class="p-2 bg-white rounded d-flex">
 							<div class="flex-grow-1">
 								<button class="btn btn-danger" id='exitButton' onclick="rc.exit()">
