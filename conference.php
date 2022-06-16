bdfcdb3dd24ee71e3c71a21fe33d70de05426319
kj6560 <kj6560@gmail.com>
2022-06-16 19:23:08 +0530

    lkslkfs

--- conference.php
+++ conference.php
@@ -81,10 +81,10 @@ if ($conn && $user_id) {
 						position: absolute;
 						right: 0;
 						bottom: 0;
-						min-width: 100%;
-						min-height: 100%;
-						width: auto;
-						height: auto;
+						min-width: 1%;
+						min-height: 1%;
+						width: 25%;
+						height: 25%;
 						z-index: -100;
 						background-size: cover;
 						overflow: hidden;
