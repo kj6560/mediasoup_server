731fe0518182e3f15b96a5757203b2f61995a0b5
kj6560 <kj6560@gmail.com>
2022-07-09 01:14:53 +0530

    some

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use Symfony\Component\Routing\RouteCollection;
 
-class PageController extends Controller
+class UsersController extends Controller
 {
 
     public function add_client(RouteCollection $routes)
